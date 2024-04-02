5238d9ad83191f98939ae19c58223e8d09221555
KatWei67 <jwei7@uw.edu>
2024-04-02 02:39:24 -0700

    Finish exercise 3

--- ECON481_Homework/ECON481_PS1.py
+++ ECON481_Homework/ECON481_PS1.py
@@ -26,10 +26,34 @@ def evens_and_odds(n: int) -> dict:
 
     Return:
     A dictionary with two keys 'evens' & 'odds', and two valus 'sum of evens' & 'sum of odds'
+
+    For example, evens_and_odds(4) should return {'evens': 2, 'odds': 4}
     """
     evens = sum(i for i in range(n) if i % 2 == 0)
     odds = sum (i for i in range(n) if i % 2 != 0)
 
     return {'evens': evens, 'odds': odds}
 
+
 ### Exercise 3
+from typing import Union
+from datetime import datetime
+def time_diff(date_1: str, date_2: str, out: str) -> Union[str,float]:
+    """
+    If keyword is float, then return the absolute value in days.
+    If keyword is string, then return a sentance that there are X days between the two dates.
+
+    For example, time_diff('2020-01-01', '2020-01-02', 'float') should return 1
+    For example, time_diff('2020-01-03', '2020-01-01', 'string') should return
+    "There are 2 days between the two dates"
+    """
+    d1 = datetime.strptime(date_1, '%Y-%m-%d')
+    d2 = datetime.strptime(date_2, '%Y-%m-%d')
+    different_days = abs(d1.day - d2.day)
+    if out == 'float':
+        return float(different_days)
+    else:
+        return f"There are {different_days} between the two dates"
+    
+#### Exercise 4
+
