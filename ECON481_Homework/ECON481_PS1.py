464d80885cb0dedf0821c84bbf13701f22be5571
KatWei67 <jwei7@uw.edu>
2024-04-02 01:53:03 -0700

    Finish exercise 0

---
+++ ECON481_Homework/ECON481_PS1.py
@@ -0,0 +1,15 @@
+### Problem Set 1
+# Jiaying (Katherine) Wei
+
+### Exercise 0
+# Please write a function that takes no arguments and returns a link to your solutions on GitHub.
+# Use the following shell:
+def github() -> str:
+    """
+    This function takes no argument and returns a link to my homework solution on Github.
+    """
+
+    return "https://github.com/KatWei67/ECON481-Homework"
+
+github()
+
