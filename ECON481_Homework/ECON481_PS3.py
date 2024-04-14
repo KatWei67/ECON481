9de1645ca5090446b3fec170881b11ddb8451ed0
KatWei67 <jwei7@uw.edu>
2024-04-14 16:33:14 -0700

    test

---
+++ ECON481_Homework/ECON481_PS3.py
@@ -0,0 +1,36 @@
+### Exercise 0
+def github() -> str:
+    """
+    Some docstrings.
+    """
+
+    return "https://github.com/<user>/<repo>/blob/main/<filename.py>"
+
+### Exercise 1
+import pandas as pd
+
+def import_yearly_data(years: list) -> pd.DataFrame:
+    """
+    Some docstrings.
+    """
+
+    return None
+
+### Exercise 2
+import pandas as pd
+
+def import_parent_companies(years: list) -> pd.DataFrame:
+    """
+    Some docstrings.
+    """
+
+    return None
+
+### Exercise 3
+def n_null(df: pd.DataFrame, col: str) -> int:
+    """
+    Some docstrings
+    """
+
+    return None
+
